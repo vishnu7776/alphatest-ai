4eee8242a8a3b5368a3aee47c330a5ffbe97538f
vishnu sankar <sankarvishnu64@gmail.com>
2025-09-21 09:25:21 +0000

    /test-cases/HC-REQ-001
    in test case spage detailed
    
    each test cases

--- src/app/(main)/test-cases/[id]/page.tsx
+++ src/app/(main)/test-cases/[id]/page.tsx
@@ -19,6 +19,7 @@ import { BackButton } from '@/components/back-button';
 import { SwaggerIntegration } from '@/components/swagger-integration';
 import { testCaseData } from '@/lib/test-case-data';
 import type { Requirement, TestCase } from '@/lib/test-case-data';
+import { Button } from '@/components/ui/button';
 
 
 const getStatusIcon = (status: TestCase['status']) => {
@@ -49,6 +50,18 @@ const getStatusBadge = (status: TestCase['status']) => {
   }
 };
 
+const JiraIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path fillRule="evenodd" clipRule="evenodd" d="M12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0ZM12.3379 12.0003L16.4852 7.85291C16.8049 7.53326 16.8049 7.0229 16.4852 6.70325C16.1656 6.3836 15.6552 6.3836 15.3356 6.70325L10.5093 11.5296L8.66469 9.685C8.34504 9.36535 7.83468 9.36535 7.51503 9.685C7.19538 10.0047 7.19538 10.515 7.51503 10.8347L11.1883 14.508L7.51503 18.1813C7.19538 18.5009 7.19538 19.0113 7.51503 19.3309C7.83468 19.6506 8.34504 19.6506 8.66469 19.3309L12.3379 15.6577L15.3356 18.6553C15.6552 18.975 16.1656 18.975 16.4852 18.6553C16.8049 18.3357 16.8049 17.8253 16.4852 17.5057L12.3379 12.0003Z" fill="#2684FF"/>
+    </svg>
+);
+
+const AzureDevOpsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M18.4483 0.00012207L12.0689 3.25012L7.35166 0.00012207L0 10.0001V22.0001L7.36896 15.5001L12.0862 19.0001L18.431 22.0001L24 12.0001V3.25012L18.4483 0.00012207ZM7.36896 3.86212L10.362 5.60362L7.36896 8.50012V3.86212Z" fill="#0078D4"/>
+    </svg>
+);
+
 
 const RequirementTestCases = ({ requirement }: { requirement: Requirement }) => {
     return (
@@ -68,7 +81,17 @@ const RequirementTestCases = ({ requirement }: { requirement: Requirement }) => 
                                     </div>
                                     <p className="font-semibold text-foreground mt-1">{tc.title}</p>
                                 </div>
-                                {getStatusBadge(tc.status)}
+                                <div className="flex items-center gap-2">
+                                    <Button variant="outline" size="sm" className="h-8">
+                                        <JiraIcon className="mr-2"/>
+                                        Jira
+                                    </Button>
+                                     <Button variant="outline" size="sm" className="h-8">
+                                        <AzureDevOpsIcon className="mr-2"/>
+                                        Azure
+                                    </Button>
+                                    {getStatusBadge(tc.status)}
+                                </div>
                             </div>
                             <div className="grid md:grid-cols-2 gap-4 text-xs border-t pt-3">
                                 <div>
